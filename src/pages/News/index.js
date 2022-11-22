10fef3a282338f43e80540ca7dc79a8e70195567
Lê Quang Thắng <lthang052@gmail.com>
2022-11-22 09:01:40 +0700

    add readnews page

--- src/pages/News/index.js
+++ src/pages/News/index.js
@@ -2,44 +2,55 @@ import styles from "./News.module.css";
 import images from "../../assets/image";
 import Card from "./components/Card";
 import SmallerCard from "./components/SmallerCard";
+import { Link } from "react-router-dom";
 
 const newsss = [
   {
-    title: "New car for Toyota 2022!",
+    id: 1,
+    title: "New car for Toyota 2022! 1",
     image: images.newsImg,
     date: "31-8-2022",
+    author: "Person A",
     description:
       "Hiện tại ở BMW tại Hà Tĩnh chưa có Showroom chính thức của BMW. Do vậy mọi nhu cầu về Trải nghiệm lái thử, hoặc mua xe quý khách hàng vui lòng liên hệ BMW Việt...",
     readCount: "5",
   },
   {
-    title: "New car for Toyota 2022!",
+    id: 2,
+    title: "New car for Toyota 2022! 2",
     image: images.newsImg,
     date: "31-8-2022",
+    author: "Person A",
     description:
       "Hiện tại ở BMW tại Hà Tĩnh chưa có Showroom chính thức của BMW. Do vậy mọi nhu cầu về Trải nghiệm lái thử, hoặc mua xe quý khách hàng vui lòng liên hệ BMW Việt...",
     readCount: "4",
   },
   {
-    title: "New car for Toyota 2022!",
+    id: 3,
+    title: "New car for Toyota 2022! 3",
     image: images.newsImg,
     date: "31-8-2022",
+    author: "Person A",
     description:
       "Hiện tại ở BMW tại Hà Tĩnh chưa có Showroom chính thức của BMW. Do vậy mọi nhu cầu về Trải nghiệm lái thử, hoặc mua xe quý khách hàng vui lòng liên hệ BMW Việt...",
     readCount: "3",
   },
   {
-    title: "New car for Toyota 2022!",
+    id: 4,
+    title: "New car for Toyota 2022! 4",
     image: images.newsImg,
     date: "31-8-2022",
+    author: "Person A",
     description:
       "Hiện tại ở BMW tại Hà Tĩnh chưa có Showroom chính thức của BMW. Do vậy mọi nhu cầu về Trải nghiệm lái thử, hoặc mua xe quý khách hàng vui lòng liên hệ BMW Việt...",
     readCount: "2",
   },
   {
-    title: "New car for Toyota 2022!",
+    id: 5,
+    title: "New car for Toyota 2022! 5",
     image: images.newsImg,
     date: "31-8-2022",
+    author: "Person A",
     description:
       "Hiện tại ở BMW tại Hà Tĩnh chưa có Showroom chính thức của BMW. Do vậy mọi nhu cầu về Trải nghiệm lái thử, hoặc mua xe quý khách hàng vui lòng liên hệ BMW Việt...",
     readCount: "1",
@@ -54,13 +65,18 @@ function News() {
         <section className={styles.section}>
           {newsss.map((news, index) => {
             return (
-              <Card
+              <Link
+                to={`/readnews/${news.id}`}
                 key={index}
-                image={news.image}
-                title={news.title}
-                date={news.date}
-                description={news.description}
-              />
+                className={styles.link}
+              >
+                <Card
+                  image={news.image}
+                  title={news.title}
+                  date={news.date}
+                  description={news.description}
+                />
+              </Link>
             );
           })}
         </section>
@@ -69,7 +85,13 @@ function News() {
           <h3>Most read</h3>
           {newsss.map((news, index) => {
             return (
-              <SmallerCard key={index} image={news.image} title={news.title} />
+              <Link
+                to={`/readnews/${news.id}`}
+                key={index}
+                className={styles.link}
+              >
+                <SmallerCard image={news.image} title={news.title} />
+              </Link>
             );
           })}
         </aside>
