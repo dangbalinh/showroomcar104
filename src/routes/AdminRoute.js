6b5e676983c15c1ccff8dda55af69a8c0bd43a89
Trung <thanhtrungvnger@gmail.com>
2022-12-04 16:22:29 +0700

    phan quyen vao trag admin

--- src/routes/AdminRoute.js
+++ src/routes/AdminRoute.js
@@ -1,6 +1,13 @@
-function AdminRoute() {
+import { Navigate } from "react-router-dom";
+
+function AdminRoute({children}) {
+    let user = JSON.parse(localStorage.getItem("user"));
+    console.log(user.role);
+
     return ( 
-        <></>
+        <div>
+            {((user.role === "admin" || user.role === "employee") && localStorage.getItem("token")) ? children : <Navigate to="/" /> } 
+        </div>
      );
 }
 
